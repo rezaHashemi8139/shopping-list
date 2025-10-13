8d9d8db657f9b5ee081e776d586862344f84eea6
Reza Hashemi <reza.8139@gmail.com>
2025-10-13 10:10:19 +0330

    feat : add new modules

---
+++ modules/user/user.go
@@ -0,0 +1,10 @@
+package user
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"uniqueIndex;not null"`
+	Email    string `gorm:"uniqueIndex;not null"`
+	Password string `gorm:"not null"` // hashed password
+}
